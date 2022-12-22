d5a4133af78667c0a64d47f7548c36059cb37e5c
Sabina-star <83788641+Sabina-star@users.noreply.github.com>
2022-12-22 13:23:42 +0600

    echo

--- itisyourbday.py
+++ itisyourbday.py
@@ -7,11 +7,13 @@ from time import sleep
 bot = Bot(token='5747825338:AAErPHwrsyW26ffF31MHSORVW6Pmb9thk7k')
 dp = Dispatcher(bot)
 
-answers = []  # store the answers they have given
+@dp.message_handler(commands=['start'])
+async def welcome(message: types.Message):
+	await message.reply("Hi! I am it is your birthday bot!")
 
-
-### add stuff here
-
+@dp.message_handler()
+async def echo(message: types.Message):
+	await message.answer(message.text)
 
 # this is the last line
 executor.start_polling(dp)
